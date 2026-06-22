5eb744be1a82b2da126e2911357db064d885970e
Yasin2431 <lavvafiyasin2431@gmail.com>
2026-06-22 15:08:29 +0330

    add class Bimaran

---
+++ yasin/Bimaran.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace yasin
+{
+    public class Bimaran
+    {
+        public String FirstName {  get; set; }
+        public String LastName { get; set; }
+        public String NationalCode { get; set; }
+        public String NumberPhone { get; set; }
+    }
+}
