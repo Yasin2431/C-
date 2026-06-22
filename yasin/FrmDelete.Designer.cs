dd404f9d614a93008e2e6a026e9ec70c43d4d841
Yasin2431 <lavvafiyasin2431@gmail.com>
2026-06-22 16:57:58 +0330

    Delete patient registration logic

---
+++ yasin/FrmDelete.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace yasin
+{
+    partial class FrmDelete
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txt_Nationalcode = new TextBox();
+            btn_Delete = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // txt_Nationalcode
+            // 
+            txt_Nationalcode.Location = new Point(249, 101);
+            txt_Nationalcode.Name = "txt_Nationalcode";
+            txt_Nationalcode.Size = new Size(125, 27);
+            txt_Nationalcode.TabIndex = 0;
+            txt_Nationalcode.TextChanged += txt_Nationalcode_TextChanged;
+            // 
+            // btn_Delete
+            // 
+            btn_Delete.Location = new Point(249, 134);
+            btn_Delete.Name = "btn_Delete";
+            btn_Delete.Size = new Size(125, 37);
+            btn_Delete.TabIndex = 1;
+            btn_Delete.Text = "حذف";
+            btn_Delete.UseVisualStyleBackColor = true;
+            btn_Delete.Click += btn_Delete_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Enabled = false;
+            label1.Location = new Point(249, 104);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 20);
+            label1.TabIndex = 2;
+            label1.Text = "کدملی";
+            // 
+            // FrmDelete
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(btn_Delete);
+            Controls.Add(txt_Nationalcode);
+            Name = "FrmDelete";
+            Text = "FrmDelete";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txt_Nationalcode;
+        private Button btn_Delete;
+        private Label label1;
+    }
+}
---
+++ yasin/FrmDelete.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace yasin
+{
+    public partial class FrmDelete : Form
+    {
+        public FrmDelete()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_Nationalcode_TextChanged(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            
+            Bimaran patientToDelete = Bimar.FirstOrDefault(x => x.NationalCode == txt_Nationalcode.Text.Trim());
+
+
+            if (patientToDelete != null)
+            {
+                Bimar.Remove(patientToDelete);
+                MessageBox.Show("بیمار با موفقیت از لیست حذف شد.");
+            }
+            else
+            {
+                
+                MessageBox.Show("بیماری با این کد ملی در لیست یافت نشد!");
+            }
+        }
+
+    }
+
+}
